b3f9b09e492d258ffe3e69593b704c2efe1b6f72
Ritesh Kudalkar <kriteshkudalkar@gmail.com>
2023-11-24 08:44:11 +0530

    Add more to Count Sorted Vowel Strings

--- src/main18.rs
+++ src/main18.rs
@@ -1,9 +1,31 @@
+use std::collections::HashMap;
+
+fn count_vowel_strings_for_character<'a>(map: &mut HashMap<&'a str, i32>, vow_ar: &Vec<char>, index: usize, n: i32) -> i32 {
+    if n == 1 {
+        return 1;
+    }
+    let va = format!("{}, {n}", vow_ar[index]);
+    let v: &'a str = va.clone().as_str();
+    if map.contains_key(v) {
+        return *map.get(v).unwrap();
+    }
+    let mut count = 0;
+    for i in index..vow_ar.len() {
+        count += count_vowel_strings_for_character(map, vow_ar, i, n - 1);
+    }
+    map.insert(v , count);
+    return count;
+}
 
 fn count_vowel_strings(n: i32) -> i32 {
     // a, e, i, o, u
     // aaa, aae, aai, aao, aau, aee, aei, aeo, aeu, eee, eei, eeu, eeo, eii, eio, eiu,
+    let mut map: HashMap<&str, i32> = HashMap::new();
+    let vow_ar = vec!['a', 'e', 'i', 'o', 'u'];
+    return count_vowel_strings_for_character(&mut map, &vow_ar, 0, n);
 }
 
 pub fn main18() {
-    //
+    let n = 1;
+    println!("n: {n}, count_vowel_strings: {}", count_vowel_strings(n));
 }
