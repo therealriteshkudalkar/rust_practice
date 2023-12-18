66822750e4dc3922866fbdfb852a01ca0354131f
Ritesh Kudalkar <kriteshkudalkar@gmail.com>
2023-12-18 21:10:54 +0530

    Complete Reverse Words in a String

--- src/main23.rs
+++ src/main23.rs
@@ -1,31 +1,23 @@
 
-pub fn reverse_words(s: &String) -> String {
-    let mut rev_string = String::new();
-    let mut vec_stack: Vec<String> = Vec::new();
-    let mut word = String::new();
-    for (index, ch) in s.trim().chars().enumerate() {
-        if ch == ' ' || index == s.len() - 1 {
-            if ch != ' ' {
-                word.push(ch);
-            }
-            // then a word has completed to append that word to the vec_stack
-            vec_stack.push(word.clone());
-            word.clear();
+fn reverse_words(s: &String) -> String {
+    let mut splits: Vec<&str> = s.split(" ").collect();
+    let mut string = String::new();
+    for &split in splits.iter().rev() {
+        if split.len() == 0 {
             continue;
         }
-        word.push(ch);
+        string.push_str(split);
+        string.push(' ');
     }
-    while !vec_stack.is_empty() {
-        // pop each element from the stack and push it
-        rev_string.push_str(&*vec_stack.pop().unwrap());
-        rev_string.push(' ');
-    }
-    return rev_string;
+    string.pop();
+    return string;
 }
 
 pub fn main23() {
     let s = String::from("the sky is blue");
     println!("s: {s}, reverse_words: {}", reverse_words(&s));
     let s = String::from("  hello world  ");
+    println!("s: {s}, reverse_words: {}", reverse_words(&s));
+    let s = String::from("a good   example");
     println!("s: {s}, reverse_words: {}", reverse_words(&s));
 }
